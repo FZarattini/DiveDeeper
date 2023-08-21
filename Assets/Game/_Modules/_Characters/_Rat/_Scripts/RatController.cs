03646a4ce9a9d04491e8bef7ab4a8be8b554087d
Lucas Fernandes <chapzlucas@gmail.com>
2023-08-21 14:22:11 -0300

    feat: Rat look direction

--- Assets/Game/_Modules/_Characters/_Rat/_Scripts/RatController.cs
+++ Assets/Game/_Modules/_Characters/_Rat/_Scripts/RatController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class RatController : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
     [SerializeField] private Transform followTarget;
 
     [SerializeField, Range(0, 10)] private float followDistance;
     [SerializeField, Range(0, 10)] private float followSpeed;
 
+    private Vector2 directionToTarget;
     private float distanceToTarget;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
+        LookDirection();
+
         distanceToTarget = Vector2.Distance(followTarget.position, transform.position);
 
         if (distanceToTarget < followDistance)
@@ -18,5 +28,12 @@ public class RatController : MonoBehaviour
 
         // Move the pet towards the target position
         transform.position = Vector2.MoveTowards(transform.position, followTarget.position, followSpeed * Time.deltaTime);
+    }
+
+    private void LookDirection()
+    {
+        directionToTarget = followTarget.position - transform.position;
+
+        spriteRenderer.flipX = directionToTarget.x < 0;
     }
 }
