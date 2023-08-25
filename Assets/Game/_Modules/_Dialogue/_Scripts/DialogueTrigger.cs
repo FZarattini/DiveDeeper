23214def30a11d9415622c54905a4a5ec1df2c5f
Felipe Zarattini <felipezarattini02@hotmail.com>
2023-08-24 22:03:49 -0300

    basement dialogue triggers

--- Assets/Game/_Modules/_Dialogue/_Scripts/DialogueTrigger.cs
+++ Assets/Game/_Modules/_Dialogue/_Scripts/DialogueTrigger.cs
@@ -2,17 +2,41 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour, IInteractables
 {
+    [SerializeField] bool isAutoTrigger;
     [SerializeField] protected DialogueSO _dialogue;
+    [SerializeField, ReadOnly] bool canShowDialogue; 
 
     public static Action<DialogueSO, Action> OnDialogueInteracted = null;
 
+    private void Start()
+    {
+        canShowDialogue = true;
+    }
+
     public virtual void Interact()
     {
+        if (isAutoTrigger) return;
+
         OnDialogueInteracted?.Invoke(_dialogue, null);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isAutoTrigger || !canShowDialogue) return;
+        canShowDialogue = false;
+
+        OnDialogueInteracted?.Invoke(_dialogue, null);
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canShowDialogue = true;
     }
 
 }
