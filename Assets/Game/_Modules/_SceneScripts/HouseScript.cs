b81c210d790c63fa69e1558305f37557ae382549
Felipe Zarattini <felipezarattini02@hotmail.com>
2023-08-26 17:17:40 -0300

    feat: House 3

--- Assets/Game/_Modules/_SceneScripts/HouseScript.cs
+++ Assets/Game/_Modules/_SceneScripts/HouseScript.cs
@@ -2,11 +2,15 @@ using Doozy.Runtime.UIManager.Containers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class HouseScript : MonoBehaviour
+public class HouseScript : MonoBehaviour, IInteractables
 {
     [SerializeField] UIContainer _fadeContainer;
     [SerializeField] float timeToWait;
+    [SerializeField] GameObject playerNormal;
+    [SerializeField] GameObject playerLyingDown;
+    [SerializeField] DialogueTrigger _dialogueTrigger;
 
     private void Start()
     {
@@ -20,5 +24,37 @@ public class HouseScript : MonoBehaviour
         yield return new WaitForSeconds(timeToWait);
 
         _fadeContainer.Show();
+
+        yield return new WaitForSeconds(timeToWait);
+
+        _fadeContainer.Hide();
+    }
+
+    public void SwitchCharacters()
+    {
+        playerLyingDown.SetActive(false);
+        playerNormal.SetActive(true);
+    }
+
+    void DialogueCallback()
+    {
+        StartCoroutine("ShowFadeAndLoadGraveyard");
+    }
+
+    IEnumerator ShowFadeAndLoadGraveyard()
+    {
+        _fadeContainer.Show();
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Graveyard");
+    }
+
+    public void PlayDiaryDialogue()
+    {
+        _dialogueTrigger.PlayDialogue(DialogueCallback);
+    }
+
+    public void Interact()
+    {
+        PlayDiaryDialogue();
     }
 }
