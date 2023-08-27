10aeefb2d094a3edd7304eb8d960ffe4bd1aecf3
Felipe Zarattini <felipezarattini02@hotmail.com>
2023-08-26 22:44:07 -0300

    some adjustments

--- Assets/Game/_Modules/_Misc/_Scripts/DontDestroyOnLoad.cs
+++ Assets/Game/_Modules/_Misc/_Scripts/DontDestroyOnLoad.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    public static DontDestroyOnLoad Instance;
+
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
         DontDestroyOnLoad(this);
     }
 }
